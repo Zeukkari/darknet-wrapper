4d2386a9e346e5542a2e49c48011741c0862e146
Timo Aho <timo.aho@siili.com>
2017-05-15 22:04:28 +0300

    tinkering

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ var execSync = require('child_process').execSync;
 var templateFile = "./template/vgg-conv.cfg.template";
 var template = fs.readFileSync(templateFile, 'utf-8');
 
+var argsTemplateFile = "./template/args.template";
+var argsTemplate = fs.readFileSync(argsTemplateFile, 'utf-8');
+
+var defaultArgs = {
+    range: 1,
+    iters: 15,
+    rate: 0.05,
+    rounds: 1,
+    octaves: 4,
+    thres: 1.0,
+    zoom: 1.0,
+    rotate: 0.0
+}
+
+var darknetArgs = mustache.render(argsTemplate,defaultArgs);
+console.log("darknetArgs: ", darknetArgs);
+
 if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " path/to/directory");
     process.exit(-1);
@@ -21,11 +38,11 @@ function searchDir(path) {
             var data = fs.readFileSync(pathname);
             var info = imageinfo(data);
             var fileType = info.mimeType;
-            if(fileType == "image/png" || fileType == "image/jpg") {
+            if(fileType == "image/png" || fileType == "image/jpeg") {
                 console.log(pathname +  "  Dimensions:", info.width, "x", info.height);
                 results.push({file: pathname, width: info.width, height: info.height});
             } else {
-                console.log(pathname + " - skipped");
+                console.log(pathname + " - skipped (filetype: " + fileType + ")");
             }
         }
     }
@@ -58,7 +75,7 @@ results.forEach(function(item) {
     var filename = item.file;
     var output = mustache.render(template,view);
     fs.writeFileSync('./tmp/vgg-conv.cfg', output);
-    var cmd = "./darknet nightmare tmp/vgg-conv.cfg vgg-conv.weights \"" + filename + "\" 15 && mv *png results";
+    var cmd = "./darknet nightmare tmp/vgg-conv.cfg vgg-conv.weights \"" + filename + "\" 15 " + darknetArgs + " && mv *png results";
     console.log("exec " + cmd);
     execSync(cmd);
     fs.unlinkSync('./tmp/vgg-conv.cfg');
